80a16be98f907d9d674c82673b9414a7381a99a3
BangYuWen <bangyuwen@gmail.com>
2016-07-09 08:39:09 +0800

    POSTfinal

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,4 @@
-var exec = require("child_process").exec;
+var querystring = require('querystring');
 
 function start(response) {
   console.log("Request handler 'start' was called.");
@@ -16,16 +16,15 @@ function start(response) {
     '</body>'+
     '</html>';
 
-  function (error, stdout, stderr) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
     response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent " + querystring.parse(postData).text);
   response.end();
 }
 
